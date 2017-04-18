f0b604ef2d21ca46e819f1ff094ba40c3cd1c899
nieldeokar <Niel@Nileshs-MacBook-Pro.local>
2017-04-18 17:04:59 +0530

    completedPieProgressView

---
+++ app/src/main/java/com/niel/circularprogresscustomview/Circle.java
@@ -0,0 +1,63 @@
+package com.niel.circularprogresscustomview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Circle extends View {
+
+    private static final int START_ANGLE_POINT = 270;
+
+    private final Paint paint;
+    private final RectF rect;
+
+    private float angle;
+    private int color;
+
+    public Circle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        final int strokeWidth = 200;
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(strokeWidth);
+        //Circle color
+        paint.setColor(Color.RED);
+
+
+
+
+        //size 200x200 example
+        rect = new RectF(strokeWidth, strokeWidth, 200 + strokeWidth, 200 + strokeWidth);
+
+        //Initial Angle (optional, it can be zero)
+        angle = 0;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //paint.setColor(color);
+        canvas.drawArc(rect, START_ANGLE_POINT, angle, false, paint);
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+}
--- app/src/main/java/com/niel/circularprogresscustomview/MainActivity.java
+++ app/src/main/java/com/niel/circularprogresscustomview/MainActivity.java
@@ -1,12 +1,27 @@
 package com.niel.circularprogresscustomview;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
+
+
 }
