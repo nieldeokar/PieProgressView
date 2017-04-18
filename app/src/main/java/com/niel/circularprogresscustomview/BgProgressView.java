59fb6f7d9d665d8630612683c9ec6066862da6ff
nieldeokar <Niel@Nileshs-MacBook-Pro.local>
2017-04-18 18:23:33 +0530

    bgRectange view complete

---
+++ app/src/main/java/com/niel/circularprogresscustomview/BgProgressView.java
@@ -0,0 +1,93 @@
+package com.niel.circularprogresscustomview;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BgProgressView extends View {
+
+    /*private static final int DEFAULT_MAX = 100;
+    private static final int DEFAULT_PROGRESS = 0;
+    private static final int DEFAULT_START_ANGLE = -90;
+*/
+
+    private int mMax = 100;
+    private int mProgress = 0;
+    private int mStartAngle = -90;
+
+
+    private int mViewSize;
+
+    private static final int DEFAULT_VIEW_SIZE = 96;
+
+    private Paint mProgressPaint;
+    private Paint mBackgroundPaint;
+
+
+
+
+
+
+    public BgProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        final Resources res = getResources();
+
+        int backgroundColor = res.getColor(android.R.color.holo_orange_light);
+        int progressColor = res.getColor(android.R.color.white);
+
+        mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBackgroundPaint.setColor(backgroundColor);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+
+
+        mProgressPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mProgressPaint.setColor(progressColor);
+        mProgressPaint.setStyle(Paint.Style.FILL);
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        canvas.drawRect(0, 0, mViewSize, mViewSize, mBackgroundPaint);
+
+        float sweepAngle = mViewSize * mProgress / mMax;
+
+        sweepAngle = mViewSize - sweepAngle;
+
+        canvas.drawRect(0, 0, sweepAngle, mViewSize, mProgressPaint);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = resolveSize(DEFAULT_VIEW_SIZE, widthMeasureSpec);
+        int height = resolveSize(DEFAULT_VIEW_SIZE, heightMeasureSpec);
+        mViewSize = Math.min(width, height);
+
+        setMeasuredDimension(width, height);
+    }
+
+    /**
+     * Sets the current progress (must be between 0 and max).
+     */
+    public void setProgress(int progress) {
+        if (progress > mMax || progress < 0) {
+            throw new IllegalArgumentException(
+                String.format("Progress (%d) must be between %d and %d", progress, 0, mMax));
+        }
+        mProgress = progress;
+
+        invalidate();
+    }
+
+
+}
--- app/src/main/java/com/niel/circularprogresscustomview/MainActivity.java
+++ app/src/main/java/com/niel/circularprogresscustomview/MainActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -12,15 +13,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
+  private SeekBar mSeekBar;
+  private PieProgressView mProgressPieView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
+    mProgressPieView = (PieProgressView) findViewById(R.id.progressPie);
+
+    // SeekBar
+    mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+    mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        mProgressPieView.setProgress(i);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+  }
+
+
+  public void redirect(View view){
+    startActivity(new Intent(this,BgActivity.class));
   }
 
 
