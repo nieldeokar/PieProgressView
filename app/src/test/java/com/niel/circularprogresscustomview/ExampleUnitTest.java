56968a469c52734dc9a04bb36cf62c3986b267ac
nilesh <nieldeokar@gmail.com>
2018-08-09 21:25:41 +0200

    updated test

--- app/src/test/java/com/niel/circularprogresscustomview/ExampleUnitTest.java
+++ app/src/test/java/com/niel/circularprogresscustomview/ExampleUnitTest.java
@@ -13,4 +13,11 @@ public class ExampleUnitTest {
   @Test public void addition_isCorrect() throws Exception {
     assertEquals(4, 2 + 2);
   }
+
+  @Test public void array_reverse() throws Exception {
+
+    assertEquals(4, 2 + 2);
+  }
+
+
 }
